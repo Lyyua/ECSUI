5c9e452acb32764ae4e9a77916c8e3be96e62f57
MyBlog <1070663764@qq.com>
2018-01-28 18:46:23 +0800

    销毁系统

---
+++ Assets/Sources/System/Logic/DestroySystem.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using Entitas;
+using UnityEngine;
+
+/// <summary>
+/// 消费水晶时触发  
+/// </summary>
+public class DestroySystem : ReactiveSystem<UIEntity>
+{
+    private Contexts _contexts;
+
+    public DestroySystem(Contexts contexts) : base(contexts.uI)
+    {
+        _contexts = contexts;
+    }
+
+    protected override void Execute(List<UIEntity> entities)
+    {
+        foreach (var item in entities)
+        {
+            item.Destroy();
+        }
+    }
+
+    protected override bool Filter(UIEntity entity)
+    {
+        return entity.isDestroy;
+    }
+
+    protected override ICollector<UIEntity> GetTrigger(IContext<UIEntity> context)
+    {
+        return context.CreateCollector(UIMatcher.Destroy);
+    }
+}
