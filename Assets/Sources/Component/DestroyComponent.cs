4508573c4b884ab4c820bf6f00e5404655c81888
MyBlog <1070663764@qq.com>
2018-01-28 18:45:40 +0800

    组件是否需要销毁标记

---
+++ Assets/Sources/Component/DestroyComponent.cs
@@ -0,0 +1,12 @@
+﻿using Entitas;
+using Entitas.CodeGeneration.Attributes;
+using System.Collections.Generic;
+
+/// <summary>
+/// 需要的水晶数
+/// </summary>
+[UI]
+public class DestroyComponent : IComponent
+{
+}
+
