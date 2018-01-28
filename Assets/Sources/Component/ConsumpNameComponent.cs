041596f9252c7d445c2f20be68210d9d37472011
MyBlog <1070663764@qq.com>
2018-01-28 18:43:43 +0800

    技能名字组件

---
+++ Assets/Sources/Component/ConsumpNameComponent.cs
@@ -0,0 +1,13 @@
+﻿using Entitas;
+using Entitas.CodeGeneration.Attributes;
+using System.Collections.Generic;
+
+/// <summary>
+/// 技能名字
+/// </summary>
+[UI]
+public class ConsumpNameComponent : IComponent
+{
+    public string value;
+}
+
